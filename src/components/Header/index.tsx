bbd0e2172c5cbe876ab9ac9a2964a96a9fe92253
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-05-04 20:08:09 -0300

    adding header component style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,10 @@
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header className={`${commonStyles.container} ${styles.header}`}>
+      <img src="/logo.svg" alt="logo" />
+    </header>
+  );
 }
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -2,6 +2,8 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
 
+import Header from '../../components/Header';
+
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
@@ -27,19 +29,25 @@ interface PostProps {
 }
 
 export default function Post() {
-  return <h1>Post</h1>;
+  return <Header />;
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(TODO);
+  // TODO
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async context => {
+  // const prismic = getPrismicClient();
+  // const response = await prismic.getByUID(TODO);
 
-//   // TODO
-// };
+  return {
+    props: {},
+  };
+};
